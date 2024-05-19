17ac5f789f5750e96236b1608a42f62458824921
Alexandrbig1 <alexsmagin1@gmail.com>
2024-05-18 22:57:19 -0500

    feat: added license. fix: updated gitignore

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,17 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router";
 import Loader from "../components/Loader/Loader";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 function RootLayout() {
   return (
     <div>
+      <Header />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
+      <Footer />
     </div>
   );
 }
