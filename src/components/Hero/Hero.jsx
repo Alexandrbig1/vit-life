54b33b4e4cad8b3571149e560f016831a7e59294
Alexandrbig1 <alexsmagin1@gmail.com>
2024-05-22 18:06:08 -0500

    feat: added hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,20 @@
 import { HeroContainer } from "./Hero.styled";
 
 function Hero() {
-  return <HeroContainer>Hero</HeroContainer>;
+  return (
+    <HeroContainer>
+      <div>
+        <h1>Discover innovative ways to decorate</h1>
+        <p>
+          We provide unmatched quality, comfort, and style for property owners
+          across the country. Our experts combine form and function in bringing
+          your vision to life. Create a room in your own style with our
+          collection and make your property a reflection of you and what you
+          love.
+        </p>
+      </div>
+    </HeroContainer>
+  );
 }
 
 export default Hero;
